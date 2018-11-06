49450a016e544669cdee379e27ca5df6614982d4
Tim Siwula <tcsiwula@gmail.com>
2018-11-05 19:55:20 -0600

    change to dark theme

--- frontend/App.js
+++ frontend/App.js
@@ -3,15 +3,20 @@ import {StyleSheet, Text, View} from 'react-native';
 export default class App extends React.Component {
   render() {
     return (<View style={styles.container}>
-      <Text>Mojo Hello World</Text>
+      <Text style={styles.mediumWhite}>Mojo Hello World</Text>
     </View>);
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  mediumWhite: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30
   }
 });
