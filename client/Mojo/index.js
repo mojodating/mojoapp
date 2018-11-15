82210037d411842c121abfc19bf63e124717b5a5
Tim Siwula <tcsiwula@gmail.com>
2018-11-14 21:19:45 -0600

    add splash page

--- client/Mojo/index.js
+++ client/Mojo/index.js
@@ -1,37 +1,28 @@
-// /** @format */
-// 
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-// 
-// AppRegistry.registerComponent(appName, () => App);
-
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 import Main from './src/components/Main';
+import AppNavigator from './src/app.navigator';
+
 
 export default class Mojo extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Main />
-      </View>
-    );
+        <AppNavigator/>
+    //   <View style={styles.container}>
+    //   // <Main/>
+    // </View>
+
+  );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#F5FCFF'
+//   }
+// });
+
 
 AppRegistry.registerComponent('Mojo', () => Mojo);
